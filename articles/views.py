e1c2864bd87dfa10828e231d0150c026d9a435c5
ubuntu <pipecat@yahoo.com>
2016-12-01 20:03:51 +0800

    Commit all files of my_blog

---
+++ articles/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from models import Article
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello World!")
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+def home_tmp(request):
+    post_list = Article.objects.all()
+    return render(request,'home.html',{'post_list': post_list})
+
+def detail(request,id):
+    try:
+        post = Article.objects.get(id = str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request,'detail.html',{'post': post})
+    
+        
